0112f4602e429d3778c931c8140f99308f7f5b3f
Gabriel Vieira <gabrielvieira1@protonmail.com>
2024-02-11 12:50:27 -0300

    How to use app launching and protocol handling

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+      //var uri = new Uri(@"https://www.bing.com/?cc=br");
+      var uri = new Uri(@"foodtruck:burgers");
+
+      var launchOptions = new LauncherOptions()
+      {
+        TargetApplicationPackageFamilyName = "75b5be6a-3249-46b5-b0e8-25ec0224b92c_03w1eqkrn0fpt", // App 2
+        //TargetApplicationPackageFamilyName = "b9968d3a-1d0e-4d41-ac1f-5e922d694357_03w1eqkrn0fpt", // App 3
+      };
+
+      //var success = await Launcher.LaunchUriAsync(uri, launchOptions);
+
+      var set = new ValueSet();
+      set.Add("item", "cheeseburguer");
+      var launchResult = await Launcher.LaunchUriForResultsAsync(uri, launchOptions, set);
+      if (launchResult.Status == LaunchUriStatus.Success)
+      {
+        object resultItem = null;
+        if (launchResult.Result?.TryGetValue("price", out resultItem) ?? false)
+        {
+          var dialog = new MessageDialog((string)resultItem);
+          await dialog.ShowAsync();
+        }
+      }
+    }
+
+    private async void Button_Click1(object sender, RoutedEventArgs e)
+    {
+      var handlers = await Launcher.FindUriSchemeHandlersAsync("foodtruck");
+      foreach (var handler in handlers)
+      {
+        System.Diagnostics.Debug.WriteLine($"PackageFamilyName: {handler.PackageFamilyName}");
+      }
+    }
+  }
+}
---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App2
+{
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+    }
+
+    public void SetArgs(string Uri, ValueSet data)
+    {
+      uri.Text = Uri;
+      object itemvalue = null;
+      if (data.TryGetValue("item", out itemvalue))
+        this.item.Text = (string)itemvalue;
+    }
+
+    public void SetArgsAndGetResults(string args, ValueSet data, ProtocolForResultsOperation op)
+    {
+      _op = op;
+      SetArgs(args, data);
+    }
+
+    ProtocolForResultsOperation _op;
+
+    private void sendResult_Click(object sender, RoutedEventArgs e)
+    {
+      var ResultSet = new ValueSet();
+      ResultSet.Add("price", "2.99");
+      _op.ReportCompleted(ResultSet);
+    }
+  }
+}
